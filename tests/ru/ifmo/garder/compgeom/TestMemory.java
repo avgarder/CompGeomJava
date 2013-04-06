3efd6c0324f5739093bf6d57daeefe38e9f7444f
Anton <anton@anton-lt-ub.(none)>
2013-04-07 03:06:41 +0400

    Modified TestMemory

--- tests/ru/ifmo/garder/compgeom/TestMemory.java
+++ tests/ru/ifmo/garder/compgeom/TestMemory.java
@@ -6,15 +6,23 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import ru.ifmo.garder.compgeom.primitives.Point2;
 
+import static org.junit.Assert.*;
+
 @RunWith(JUnit4.class)
 public class TestMemory {
-    private static final int N = (int) 1e2;
+    private static final int N = (int) 1e7;
 
     @Test
     public void testPoint2() {
         Point2<Float64>[] points = new Point2[N];
         for (int i = 0; i < N; i++) {
-            points[i] = new Point2<Float64>(Float64.ZERO, Float64.ZERO);
+            points[i] = new Point2<Float64>(Float64.valueOf(i), Float64.valueOf(i));
+        }
+
+        for (int i = 0; i < N; i++) {
+            assertNotNull(points[i]);
+            assertNotNull(points[i].x);
+            assertNotNull(points[i].y);
         }
     }
 }
